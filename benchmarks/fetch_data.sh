c97d511a901624d2b238870102c13609e14a4d0d
Max Meldrum <max@meldrum.se>
2024-03-04 14:59:09 +0100

    Update script for new NYC Citi Bike URL

--- benchmarks/fetch_data.sh
+++ benchmarks/fetch_data.sh
@@ -2,22 +2,28 @@
 
 mkdir -p data
 
-urls=("https://s3.amazonaws.com/tripdata/201808-citibike-tripdata.csv.zip" "https://s3.amazonaws.com/tripdata/201809-citibike-tripdata.csv.zip" "https://s3.amazonaws.com/tripdata/201810-citibike-tripdata.csv.zip" "https://s3.amazonaws.com/tripdata/201811-citibike-tripdata.csv.zip" "https://s3.amazonaws.com/tripdata/201812-citibike-tripdata.csv.zip")
+echo "Preparing NYC Citi Bike Dataset"
 
-for url in "${urls[@]}"; do
-    file=`basename "$url"`
-    wget "$url" -O data/"$file"
-    unzip data/"$file" -d data/
-    rm data/"$file"
-done
+bike_url="https://s3.amazonaws.com/tripdata/2018-citibike-tripdata.zip"
+wget "$bike_url" -P data/
+file=`basename "$bike_url"`
+unzip data/"$file" -d data/
 
+# urls=("https://s3.amazonaws.com/tripdata/201808-citibike-tripdata.csv.zip" "https://s3.amazonaws.com/tripdata/201809-citibike-tripdata.csv.zip" "https://s3.amazonaws.com/tripdata/201810-citibike-tripdata.csv.zip" "https://s3.amazonaws.com/tripdata/201811-citibike-tripdata.csv.zip" "https://s3.amazonaws.com/tripdata/201812-citibike-tripdata.csv.zip")
+
+# for url in "${urls[@]}"; do
+#     file=`basename "$url"`
+#     wget "$url" -O data/"$file"
+#     unzip data/"$file" -d data/
+#     rm data/"$file"
+# done
+
+input_files=("data/2018-citibike-tripdata/201808-citibike-tripdata.csv" "data/2018-citibike-tripdata/201809-citibike-tripdata.csv" "data/2018-citibike-tripdata/201810-citibike-tripdata.csv" "data/2018-citibike-tripdata/201811-citibike-tripdata.csv" "data/2018-citibike-tripdata/201812-citibike-tripdata.csv")
 output_file="data/citibike-tripdata.csv"
-input_files=("data/201808-citibike-tripdata.csv" "data/201809-citibike-tripdata.csv" "data/201810-citibike-tripdata.csv" "data/201811-citibike-tripdata.csv" "data/201812-citibike-tripdata.csv")
 
 touch data/citibike-tripdata.csv
 
 head -n 1 "${input_files[0]}" > "$output_file"
-
 
 for input_file in "${input_files[@]}"; do
     tail -n +2 "$input_file" >> "$output_file"
